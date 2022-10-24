e310c5abcd6dc6ecee72ca324708c3ee1534a8f2
inaki.aramburur <inaki.aramburur@alumni.mondragon.edu>
2022-10-24 11:22:41 +0200

    supermarket

--- Monitors/supermarket/src/main/java/edu/mondragon/os/monitors/supermarket/Checkout.java
+++ Monitors/supermarket/src/main/java/edu/mondragon/os/monitors/supermarket/Checkout.java
@@ -19,23 +19,38 @@ public class Checkout {
     }
 
     // TODO: modify this function
-    public void serve(Cashier cashier) throws InterruptedException {
+    public synchronized void serve(Cashier cashier) throws InterruptedException {
         print(cashier, "ready");
-
+        while (!customerReady) {
+            this.wait();
+        }
         print(cashier, "serving");
         Thread.sleep(rand.nextInt(1000) + 1000);
         print(cashier, "serving done");
-
+        cashierDone = true;
+        
+        this.notify();
+        while (!customerDone) {
+            this.wait();
+        }
+        customerDone = false;
         print(cashier, "done\n");
     }
 
     // TODO: modify this function
-    public void pay(Customer customer) throws InterruptedException {
+    public synchronized void pay(Customer customer) throws InterruptedException {
         print(customer, "goes to checkout");
-
+        customerReady = true;
+        this.notify();
+        while (!cashierDone) {
+            this.wait();
+        }
+        cashierDone = false;
         print(customer, "paying");
         Thread.sleep(rand.nextInt(1000) + 100);
         print(customer, "payment done");
+        customerDone = true;
+        this.notify();
     }
 
     private void print(Thread thread, String msg) {
